a19ebfdefcba66ea0d55c82ebc288869d462059b
baughtnet <114878979+baughtnet@users.noreply.github.com>
2023-12-03 14:23:53 -0800

    telescope-file-browser plugin and config & added live grep search

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,22 @@
+require("telescope").setup {
+  defaults = {
+    theme = "center",
+    sorting_strategy = "ascending",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.6,
+      },
+    },
+  },
+}
 local builtin = require('telescope.builtin')
+require('telescope').load_extension('file_browser')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("grep > ") });
+
 end)
